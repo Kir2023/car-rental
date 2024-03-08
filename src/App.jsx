5810e50211d0e90bbd5e27c2818b4feee4ede0e2
Kir2023 <132088682+Kir2023@users.noreply.github.com>
2024-03-08 22:57:34 +0200

    Fix index.html

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,13 @@ const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const CatalogPage = lazy(() => import("./pages/CatalogPage/CatalogPage"));
 const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="catalog" element={<CatalogPage />} />
-        <Route path="favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<HomePage />} />
-      </Route>
-    </Routes>
-  );
-}
-
-export default App;
+export const App = () => (
+  <Routes>
+    <Route path="/" element={<SharedLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="catalog" element={<CatalogPage />} />
+      <Route path="favorites" element={<FavoritesPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  </Routes>
+);
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
